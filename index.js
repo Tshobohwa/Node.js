3086118b98c8f7c0a7b2d0ad710be0a70c83833b
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-17 12:53:42 +0200

    005 Read and write files

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
 
-const hello = 'hello world';
-console.log(hello)
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+console.log(textIn);
+
+const textout = `This is what we know about avocado: ${textIn} \n Created on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textout);
