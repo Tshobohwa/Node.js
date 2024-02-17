c28267341d6c7150d10c665cb1e46b38da42aa6a
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-17 21:13:26 +0200

    014 Using Modules 2_ Our Own Modules

--- node-farm/index.js
+++ node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate')
+
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8')
 const dataObj = JSON.parse(data)
 
@@ -9,21 +11,6 @@ const tempOverview = fs.readFileSync('./templates/template-overview.html', 'utf-
 const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
 const tempProduct = fs.readFileSync('./templates/template-product.html', 'utf-8');
 
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%ID%}/g, product.id)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-
-    !product.organic ? output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic') : output = output.replace(/{%NOT_ORGANIC%}/g, '');
-
-    return output; 
-}
 
 const server = http.createServer((req, res) => {
     const reqUrl = req.url;
