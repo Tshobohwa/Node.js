080d9f7a4f72eb94bb13ba9cf6b1f19462646bb0
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-17 19:46:46 +0200

    012 HTML Templating_ Filling the Templates

--- node-farm/index.js
+++ node-farm/index.js
@@ -3,22 +3,58 @@ const http = require('http');
 const url = require('url');
 
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8')
+const dataObj = JSON.parse(data)
+
+const tempOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8');
+const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
+const tempProduct = fs.readFileSync('./templates/template-product.html', 'utf-8');
+
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%DESCRIPTION%}/g, product.image)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%ID%}/g, product.id)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+
+    !product.organic ? output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic') : output = output.replace(/{%NOT_ORGANIC%}/g, '');
+
+    return output; 
+}
 
 const server = http.createServer((req, res) => {
     const {url} = req;
 
+    // overview page
     if (url === '/' || url === '/home') {
         res.writeHead(200, {
             'Content-type': 'text/html'
         })
+
+        const cardsHtml = dataObj.map(el => replaceTemplate( tempCard, el)).join('');
         
-        res.end('<h1>Node Farm</h1>')
-    } else if (url === '/api') {
+        res.end(tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml))
+    } 
+    else if (url === '/product') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        })
+
+        res.end(tempProduct)
+    }
+
+    // api
+    else if (url === '/api') {
         res.writeHead(200, {
             'Content-Type': 'application/json'
         })
         res.end(data);
     }
+
+    // not found
     else {
         res.writeHead(404, {
             responseStatus: {code: 404, message:"Not found"}
