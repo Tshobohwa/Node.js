0b00e31b10a55cb6f08ad2f8b962577bd56577b2
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-18 19:18:43 +0200

    004 Building Promises

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile('./dog.txt', 'utf-8', (err, data) => {
-  console.log(data);
-  superagent.get('https://dog.ceo/api/breeds/image/random').end((err, res) => {
-    if (err) return console.log(err.message);
-    fs.writeFile('./dog-img.txt', res.body.message, (err, res) => {});
+const readFilePro = (file, encrypt) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, encrypt, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
   });
-});
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject(err);
+      resolve('File written successfuly');
+    });
+  });
+};
+
+readFilePro('./dog.txt', 'utf-8')
+  .then((data) =>
+    superagent.get(`https://dog.ceo/api/breeds/${data}/image/random`)
+  )
+  .then((res) => writeFilePro('./dog-img.txt', res.body.message))
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
